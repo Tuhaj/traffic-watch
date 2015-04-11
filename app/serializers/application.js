b59bd9f2450343ad8176d48dfd8b4c71d8a91f19
piotrzientara <piotr.zientara@rebased.pl>
2015-04-11 19:41:23 +0200

    active model serializers instead of REST serializers

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,9 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
+  attrs: {
+    polylines: {embedded: 'always'},
+    markers: {embedded: 'always'},
+    points: {embedded: 'always'}
+  }
 });
